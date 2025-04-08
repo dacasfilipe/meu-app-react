7b9eebf3b9e397331f007618cd4a2e2864e692e5
dacasfilipe <ezequiasmoreira@hotmail.com>
2025-04-07 21:41:47 -0300

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Saudacao from "../components/saudacao";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Saudacao nome="Filipe" />
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -65,7 +67,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Aprenda React
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -80,7 +82,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          exemplos
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -95,7 +97,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Vá para o nextjs →
         </a>
       </footer>
     </div>
---
+++ src/components/saudacao.jsx
@@ -0,0 +1,8 @@
+export default function Saudacao({nome}) {
+  return (
+    <div>
+      <h1>Olá, {nome}!</h1>
+      <p>Bem-vindo ao meu aplicativo React!</p>
+    </div>
+  );
+}
